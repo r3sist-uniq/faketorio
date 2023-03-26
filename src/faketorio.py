15a34b65bf5a7304bb9596a3e0493d1d906ea3ef
allContributionsAreFakeIAmABot <128967083+allContributionsAreFakeIAmABot@users.noreply.github.com>
2023-03-26 18:37:52 +0530

    file change

--- src/faketorio.py
+++ src/faketorio.py
@@ -8,13 +8,9 @@ while True:
     print(pulling, 'pulled')
     adding = subprocess.run(shell=True, args=['git', 'add', '.'], capture_output=True,cwd=save_directory)
     print(adding, 'added')
-
-
     commits = subprocess.run(shell=True, args=['git', 'commit',  '-m' , f'"the time of this commit is {time.time()}"'], capture_output=True, cwd=save_directory)
     print(commits, 'committed')
-
     pushing = subprocess.run(shell=True, args=['git', 'push', '-u', 'origin', 'master'], capture_output=True, cwd=save_directory)
-    print(pushing, 'pushed')
-    
+    print(pushing, 'pushed') 
     print('goin to sleep now')
     time.sleep(60)
