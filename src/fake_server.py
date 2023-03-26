9a9ca668ba6c1fd31020e4ae6dcff53b0eaaf3bc
Aman <amanmatreja@gmail.com>
2023-03-26 14:30:07 +0530

    removed build stuff

--- src/fake_server.py
+++ src/fake_server.py
@@ -8,11 +8,7 @@ pushing = subprocess.run(shell=True, args=['git', 'push', '-u', 'origin', 'main'
 
 while True: 
     pulling = subprocess.run(shell=True, args=['git', 'pull'], capture_output=True, cwd='..\saves')
-    print(pulling, 'pushed')
     adding = subprocess.run(shell=True, args=['git', 'add', '.'], capture_output=True,cwd='..\saves')
-    print(adding, 'added')
     commits = subprocess.run(shell=True, args=['git', 'commit',  '-m' , f'"the time of this commit is {time.time()}"'], capture_output=True, cwd='..\saves')
-    print(commits, 'commited')
     pushing = subprocess.run(shell=True, args=['git', 'push', '-u', 'origin', 'main'], capture_output=True, cwd='..\saves')
-    print(pushing, 'pushed')
     time.sleep(60)
