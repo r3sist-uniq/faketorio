c2fb88aba5020e27301d9543e0b53c2b6e6531b3
Aman <amanmatreja@gmail.com>
2023-03-26 14:51:45 +0530

    arguments?

--- src/fake_server.py
+++ src/fake_server.py
@@ -1,14 +1,21 @@
 import subprocess
 import time
+import argparse
 
-adding = subprocess.run(shell=True, args=['git', 'add', '.'], capture_output=True,cwd='..\saves')
-commits = subprocess.run(shell=True, args=['git', 'commit',  '-m' , f'"the time of this commit is {time.time()} (this is the initial commit before starting)"'], capture_output=True, cwd='..\saves')
-pushing = subprocess.run(shell=True, args=['git', 'push', '-u', 'origin', 'main'], capture_output=True, cwd='..\saves')
+parser = argparse.ArgumentParser()
+parser.add_argument("-s")
+args = parser.parse_args()
+
+save_directory = args.s
+
+adding = subprocess.run(shell=True, args=['git', 'add', '.'], capture_output=True,cwd=save_directory)
+commits = subprocess.run(shell=True, args=['git', 'commit',  '-m' , f'"the time of this commit is {time.time()} (this is the initial commit before starting)"'], capture_output=True, cwd=save_directory)
+pushing = subprocess.run(shell=True, args=['git', 'push', '-u', 'origin', 'main'], capture_output=True, cwd=save_directory)
 
 
 while True: 
-    pulling = subprocess.run(shell=True, args=['git', 'pull'], capture_output=True, cwd='..\saves')
-    adding = subprocess.run(shell=True, args=['git', 'add', '.'], capture_output=True,cwd='..\saves')
-    commits = subprocess.run(shell=True, args=['git', 'commit',  '-m' , f'"the time of this commit is {time.time()}"'], capture_output=True, cwd='..\saves')
-    pushing = subprocess.run(shell=True, args=['git', 'push', '-u', 'origin', 'main'], capture_output=True, cwd='..\saves')
+    pulling = subprocess.run(shell=True, args=['git', 'pull'], capture_output=True, cwd=save_directory)
+    adding = subprocess.run(shell=True, args=['git', 'add', '.'], capture_output=True,cwd=save_directory)
+    commits = subprocess.run(shell=True, args=['git', 'commit',  '-m' , f'"the time of this commit is {time.time()}"'], capture_output=True, cwd=save_directory)
+    pushing = subprocess.run(shell=True, args=['git', 'push', '-u', 'origin', 'main'], capture_output=True, cwd=save_directory)
     time.sleep(60)
